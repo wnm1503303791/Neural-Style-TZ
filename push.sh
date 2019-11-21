2e44b7e92c9b2d0f97fab664c5406bad0a1c852e
Moyu <wnm1503303791@gmail.com>
2019-11-21 16:23:35 +0800

    2019-11-21

---
+++ push.sh
@@ -0,0 +1,23 @@
+#########################################################################
+# File Name: push.sh
+# Author: tuzhuo
+# mail: xmb028@163.com
+# Created Time: 2019年09月11日 星期三 19时56分03秒
+#########################################################################
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    pushmessage=`date +%Y-%m-%d`
+else
+    pushmessage="$*"
+fi
+
+#pushmessage=`date +%Y-%m-%d`
+
+echo ${pushmessage}
+
+git add -A
+git commit -m "${pushmessage}"
+git push origin master
+
